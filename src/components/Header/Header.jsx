86d94f43131efc45b94e807b02660135a42bd158
WilberEV <wilberesparragoza@gmail.com>
2023-05-27 21:54:14 +0200

    Navbar size fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,71 +3,103 @@ import "./Header.css";
 import { useNavigate } from "react-router-dom";
 import { images } from "../Images/Images";
 
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
 import { useDispatch, useSelector } from "react-redux";
 import { logout, userData } from "../../layouts/userSlice";
 
 export const Header = () => {
-
   const dispatch = useDispatch();
 
   const datosUserRedux = useSelector(userData);
 
-  const navigate = useNavigate()
-
+  const navigate = useNavigate();
 
   const logMeOut = () => {
-    navigate("/")
-    dispatch(logout({ credentials: {}}));
-  }
+    navigate("/");
+    dispatch(logout({ credentials: {} }));
+  };
 
   return (
     <div>
-       {!datosUserRedux?.credentials?.token ? (
-        <div className="headerBody">
-          <div className="headerLogo" onClick={() => navigate("/")}>       
-                <img src={images.logoHeader}/>
+      {!datosUserRedux?.credentials?.token ? (
+        <div>
+          <Navbar collapseOnSelect expand="lg">
+            <Container>
+              <div className="headerLogo" onClick={() => navigate("/")}>
+                <img src={images.logoHeader} />
                 <img src="https://see.fontimg.com/api/renderfont4/YaaO/eyJyIjoiZnMiLCJoIjoyMDAsInciOjEwMDAsImZzIjoyMDAsImZnYyI6IiNGRkZGRkYiLCJiZ2MiOiIjRkZGRkZGIiwidCI6MX0/SGVwaGFlc3R1cw/aspire-demibold.png" />
               </div>
-              <div className="headerOptions">
-                <div className="headerButton" onClick={() => navigate("/treatments")}>
-                  Treatments
-                </div>
-                <div className="headerButton" onClick={() => navigate("/login")}>
-                  Login
-                </div>
-                <div className="headerButton" onClick={() => navigate("/signup")}>
-                  Sign Up
-                </div>
-              </div>  
-      </div>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto"></Nav>
+                <Nav>
+                  <div
+                    className="headerButton"
+                    onClick={() => navigate("/treatments")}
+                  >
+                    Treatments
+                  </div>
+                  <div
+                    className="headerButton"
+                    onClick={() => navigate("/login")}
+                  >
+                    Login
+                  </div>
+                  <div
+                    className="headerButton"
+                    onClick={() => navigate("/signup")}
+                  >
+                    Sign Up
+                  </div>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </div>
       ) : (
-
         <div>
-          {datosUserRedux?.credentials?.user.role === "ADMIN" &&
-
+          {datosUserRedux?.credentials?.user.role === "ADMIN" && (
             <div className="adminButton" onClick={() => navigate("/admin")}>
               Admin
             </div>
-            }
+          )}
 
-            <div className="headerBody">
-            <div className="headerLogo" onClick={() => navigate("/")}>       
-            <img src={images.logoHeader}/>
-            <img src="https://see.fontimg.com/api/renderfont4/YaaO/eyJyIjoiZnMiLCJoIjoyMDAsInciOjEwMDAsImZzIjoyMDAsImZnYyI6IiNGRkZGRkYiLCJiZ2MiOiIjRkZGRkZGIiwidCI6MX0/SGVwaGFlc3R1cw/aspire-demibold.png" />
+          <div>
+            <Navbar collapseOnSelect expand="lg">
+              <Container>
+                <div className="headerLogo" onClick={() => navigate("/")}>
+                  <img src={images.logoHeader} />
+                  <img src="https://see.fontimg.com/api/renderfont4/YaaO/eyJyIjoiZnMiLCJoIjoyMDAsInciOjEwMDAsImZzIjoyMDAsImZnYyI6IiNGRkZGRkYiLCJiZ2MiOiIjRkZGRkZGIiwidCI6MX0/SGVwaGFlc3R1cw/aspire-demibold.png" />
+                </div>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav className="me-auto"></Nav>
+                  <Nav>
+                    <div
+                      className="headerButton"
+                      onClick={() => navigate("/treatments")}
+                    >
+                      Treatments
+                    </div>
+                    <div
+                      className="headerButton"
+                      onClick={() => navigate("/profile")}
+                    >
+                      Profile
+                    </div>
+                    <div className="headerButton" onClick={() => logMeOut()}>
+                      Logout
+                    </div>
+                  </Nav>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
           </div>
-          <div className="headerOptions">
-            <div className="headerButton" onClick={() => navigate("/treatments")}>
-              Treatments
-            </div>
-            <div className="headerButton" onClick={() => navigate("/profile")}>
-              Profile
-            </div>
-            <div className="headerButton" onClick={()=>logMeOut()}>
-              Logout
-            </div>
-          </div>  
-            </div>
-          </div>
+        </div>
       )}
     </div>
   );
